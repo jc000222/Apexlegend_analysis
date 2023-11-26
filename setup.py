07b94dd069ed47640e0e0642d60cbca82d2c60d5
jc000222 <1519793475@qq.com>
2023-11-25 20:03:15 -0500

    'final_version'

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='rchen2@mail.yu.edu',
     license='MIT',
     url='https://github.com/jc000222/Apexlegend_analysis',
-    packages=['src.my_package','src.img','src.auth','src.my_package.XML','src.my_package.API','src.my_package.Scrape','src.my_package.Analysis'],
+    packages=['src.my_package','src.img','src.auth','src.my_package.XML','src.my_package.API','src.my_package.Scrape','src.my_package.Analysis','src.my_package.Selenium'],
     install_requires=[
         'matplotlib>=3.0.2',
         'mplfinance>=0.12.7a4',
@@ -17,6 +17,7 @@ setup(
         'pandas-datareader>=0.7.0',
         'seaborn>=0.11.0',
         'statsmodels>=0.11.1',
-        'yfinance>=0.2.4'
+        'yfinance>=0.2.4',
+        'selenium>=4.15.2',
     ],
 )
